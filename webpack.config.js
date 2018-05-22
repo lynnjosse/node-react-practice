d52bad28ed547a3977e6c71a1fcdc815546aaefc
lynn josse <lynn.josse@monsanto.com>
2018-05-22 11:40:26 -0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+
+const path = require('path');
+const PATHS = {
+    app: path.resolve(__dirname,'app'),
+    build: path.resolve(__dirname,'build')
+};
+module.exports = {
+    entry: {
+        app: PATHS.app + "/index.js"
+    },
+    output: {
+        path: PATHS.build,
+        filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+             test: /\.css$/,
+             use: ['style-loader','css-loader']
+         }
+      ]
+    }
+};
